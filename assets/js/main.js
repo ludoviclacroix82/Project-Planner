085870cbada3c6baf61b4bfb68c42223448cf2a6
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-05-06 21:26:08 +0200

    add swipe planner

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,31 @@ svgs.forEach(svg => {
     svg.classList.add('svgLightMode')
 });
 
+let initialX = 0;
+let moveX = 0;
+let plannerOffset = 0; // Offset initial du planner
+const swipePlanner = document.querySelector('#planner');
+console.log(swipePlanner.scrollWidth - swipePlanner.offsetWidth);
+
+swipePlanner.addEventListener('touchstart', event => {
+    initialX = event.touches[0].pageX;
+    plannerOffset = swipePlanner.scrollLeft; 
+    console.log('initialx:'+ initialX);
+    console.log('plannerOffset:'+ plannerOffset);
+});
+
+swipePlanner.addEventListener("touchmove", event => {
+    let currentX = event.touches[0].pageX;
+    moveX = currentX - initialX;
+
+    console.log('move:'+moveX);
+    let newOffset = plannerOffset + moveX; 
+    console.log('newOffset:'+ newOffset);
+
+    if (newOffset >= 0 && newOffset <= (swipePlanner.scrollWidth - swipePlanner.offsetWidth)) {
+        swipePlanner.scrollLeft = newOffset;        
+    }
+});
 // async function test() {
 //     while(true) {
 //         await sleep(1000)
