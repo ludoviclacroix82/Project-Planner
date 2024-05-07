67ae315c564ebf9f57d662be2350883bc9de5336
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-05-07 09:10:15 +0200

    hop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,48 @@
-import { sleep } from "./fnc.js";
+import {sleep} from "./fnc.js";
+import {createList} from "./list.js";
+
+//INIT DATA
+const data = JSON.parse(localStorage.getItem("data") || "[]");
+if (data != null) {
+  data.forEach((element) => {
+    const parent=document.querySelector('.'+element.state)
+    createList(parent, element.name, element.description, element.date);
+  });
+}
 
 //Darkmode
-document.documentElement.setAttribute('data-theme', 'light');
-const svgs=Array.from(document.querySelectorAll('.svg'));
-svgs.forEach(svg => {
-    svg.classList.add('svgLightMode')
+let darkmode = false;
+const modeButton = document.querySelector("#mode");
+modeButton.addEventListener("click", () => {
+  if (darkmode) {
+    darkmode=false
+    document.documentElement.setAttribute("data-theme", "light");
+    const svgs = Array.from(document.querySelectorAll(".svg"));
+    svgs.forEach((svg) => {
+      svg.classList.add("svgLightMode");
+    });
+  } else {
+    darkmode=true
+    document.documentElement.setAttribute("data-theme", "dark");
+    const svgs = Array.from(document.querySelectorAll(".svg"));
+    svgs.forEach((svg) => {
+      svg.classList.add("svgDarkMode");
+    });
+  }
 });
 
-// async function test() {
-//     while(true) {
-//         await sleep(1000)
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//         svgs.forEach(svg => {
-//             svg.classList.add('svgDarkMode')
-//             svg.classList.remove('svgLightMode')
-//         });
+// const data = [
+//     {
+//         state:'todo',
+//         name:'',
+//         description:'',
+//         date:''
+//     },
+//     {
+//         state:'doing',
+//         name:'',
+//         description:'',
+//         date:''
+//     },
+// ]
 
-//         await sleep(1000)
-//         document.documentElement.setAttribute('data-theme', 'light');
-//         svgs.forEach(svg => {
-//             svg.classList.remove('svgDarkMode')
-//             svg.classList.add('svgLightMode')
-//         });
-//     }
-// }
-// test()
