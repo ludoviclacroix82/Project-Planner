b312f26c93ed0ecccf8c0bad1f1353a681fbaa9f
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-05-07 09:00:13 +0200

    module swipe.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { sleep } from "./fnc.js";
+import {swipe} from "./swipe.js";
 
 //Darkmode
 document.documentElement.setAttribute('data-theme', 'light');
@@ -7,31 +8,7 @@ svgs.forEach(svg => {
     svg.classList.add('svgLightMode')
 });
 
-let initialX = 0;
-let moveX = 0;
-let plannerOffset = 0; // Offset initial du planner
-const swipePlanner = document.querySelector('#planner');
-console.log(swipePlanner.scrollWidth - swipePlanner.offsetWidth);
-
-swipePlanner.addEventListener('touchstart', event => {
-    initialX = event.touches[0].pageX;
-    plannerOffset = swipePlanner.scrollLeft; 
-    console.log('initialx:'+ initialX);
-    console.log('plannerOffset:'+ plannerOffset);
-});
-
-swipePlanner.addEventListener("touchmove", event => {
-    let currentX = event.touches[0].pageX;
-    moveX = currentX - initialX;
-
-    console.log('move:'+moveX);
-    let newOffset = plannerOffset + moveX; 
-    console.log('newOffset:'+ newOffset);
-
-    if (newOffset >= 0 && newOffset <= (swipePlanner.scrollWidth - swipePlanner.offsetWidth)) {
-        swipePlanner.scrollLeft = newOffset;        
-    }
-});
+swipe();
 // async function test() {
 //     while(true) {
 //         await sleep(1000)
---
+++ assets/js/swipe.js
@@ -0,0 +1,36 @@
+/**
+ * Swipe : gère le défilement horizontal 
+ * Les variables initialX, moveX et plannerOffset sont déclarées pour stocker les positions initiales et de déplacement lors de l'interaction tactile.
+ * écouteur d'événements  touchstart touchmove
+ * @param {*} reverse détermine la direction du défilement. -- boolean true : drag&drop ; false swipe noraml 
+ */
+export function swipe(reverse) {
+    let initialX = 0;
+    let moveX = 0;
+    let plannerOffset = 0; // Offset initial du planner
+    const swipePlanner = document.querySelector('#planner');
+    //console.log(swipePlanner.scrollWidth - swipePlanner.offsetWidth);
+
+    swipePlanner.addEventListener('touchstart', event => {
+        initialX = event.touches[0].pageX;
+        plannerOffset = swipePlanner.scrollLeft;
+        //console.log('initialx:'+ initialX);
+        //console.log('plannerOffset:'+ plannerOffset);
+    });
+
+    swipePlanner.addEventListener("touchmove", event => {
+        let currentX = event.touches[0].pageX;
+        if (reverse)
+            moveX = currentX - initialX;
+        else
+            moveX = initialX - currentX;
+
+        //console.log('move:'+moveX);
+        let newOffset = plannerOffset + moveX;
+        //console.log('newOffset:'+ newOffset);
+
+        if (newOffset >= 0 && newOffset <= (swipePlanner.scrollWidth - swipePlanner.offsetWidth)) {
+            swipePlanner.scrollLeft = newOffset;
+        }
+    });
+}
